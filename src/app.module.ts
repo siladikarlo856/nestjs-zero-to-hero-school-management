a83fd35a9685e8b98105a2cbfebbc42203b1713c
karlo <siladikarlo856@gmail.com>
2024-01-27 00:10:31 +0100

    feat: add typeorm and mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       driver: ApolloDriver,
---
+++ src/lesson/lesson.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, ObjectIdColumn, PrimaryColumn, Column } from 'typeorm';
+
+@Entity()
+export class Lesson {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  startDate: string;
+
+  @Column()
+  endDate: string;
+}
