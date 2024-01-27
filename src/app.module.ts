fc8612b4cf3f75aa9f879bc033a55eadb07833b7
karlo <siladikarlo856@gmail.com>
2024-01-27 21:11:46 +0100

    feat: define student entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       driver: ApolloDriver,
     }),
     LessonModule,
+    StudentModule,
   ],
   controllers: [],
   providers: [],
