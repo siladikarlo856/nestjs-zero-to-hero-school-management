29ce932b7f5d1ae88384aaaf8be67899c8c2ca65
karlo <siladikarlo856@gmail.com>
2024-01-27 20:48:07 +0100

    feat: add get lesson by id

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -8,13 +8,8 @@ export class LessonResolver {
   constructor(private lessonService: LessonService) {}
 
   @Query((returns) => LessonType)
-  lesson() {
-    return {
-      id: 'asdfasdf',
-      name: 'Physics Class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
   }
 
   @Mutation((returns) => LessonType)
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -23,4 +23,8 @@ export class LessonService {
     });
     return this.lessonRepository.save(lesson);
   }
+
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({ where: { id } });
+  }
 }
